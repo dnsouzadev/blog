08ae49e624d9ebe5466887d568afd3e094a78633
daniel souza <workdndsza@gmail.com>
2022-08-22 10:34:15 -0300

    cadastro de category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('./Category')
+const slugify = require('slugify')
 
 router.get('/admin/categories/new', (req, res) => {
   res.render('admin/categories/new.ejs')
+})
+
+router.post('/categories/save', (req, res) => {
+  let { title } = req.body
+  console.log(title)
+  console.log(title.length)
+  console.log(typeof(title))
+
+  title.length !== 0 ? Category.create({
+    title: title,
+    slug: slugify(title)
+  }).then(() => {
+    res.redirect('/')
+  }) : res.redirect('/admin/categories/new')
 })
 
 module.exports = router
