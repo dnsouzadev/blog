f73be439376d457733c34d1637fed7986f75425b
daniel souza <workdndsza@gmail.com>
2022-08-23 10:36:49 -0300

    configurando o timezone para utf-3

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize' )
 
 const connection = new Sequelize('blog', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: 'utf-3'
 })
 
 module.exports = connection
