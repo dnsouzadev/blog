356c612a7d2cbacbffb5a2cb3da666b63407ed97
daniel souza <workdndsza@gmail.com>
2022-08-23 10:28:54 -0300

    adicionando um seletor de categoria na view de registro do artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../categories/Category')
 
 router.get('/articles', (req, res) => {
   res.send('rota de articles')
 })
 
 router.get('/admin/articles/new', (req, res) => {
-  res.render('admin/articles/new')
+
+  Category.findAll().then(categorias => {
+    res.render('admin/articles/new', { categories: categorias })
+  })
 })
 
 module.exports = router
