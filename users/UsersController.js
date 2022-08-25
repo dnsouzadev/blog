5ab9069b21dbd2450fe104d6463b51f565c92626
daniel souza <workdndsza@gmail.com>
2022-08-25 13:20:14 -0300

    criando usuario no banco de dados

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+
 const User = require('./User')
 
 const router = express.Router()
@@ -11,8 +13,30 @@ router.get('/admin/create', (req, res) => {
   res.render('admin/users/create')
 })
 
-router.post('users/create', (req, res) => {
+router.post('/users/create', (req, res) => {
   const { email, password } = req.body
+
+  User.findOne({ where: {
+    email
+  }}).then(user => {
+    if (user === null) {
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, salt)
+
+      User.create({
+        email,
+        password: hash
+      }).then(() => {
+        res.redirect('/')
+
+      }).catch(() => {
+        res.redirect('/admin/create')
+      })
+
+    } else {
+      res.redirect('/admin/create')
+    }
+  })
 })
 
 module.exports = router
